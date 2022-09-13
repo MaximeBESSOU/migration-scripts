c7d4636077e1b02b0818451b819f64b268725571
MaximeBESSOU <mbessou.gb@gmail.com>
2022-09-13 13:47:34 +0200

    FIX: set api:: to singular name

--- v3-sql-v4-sql/migrate/helpers/migrateValues.js
+++ v3-sql-v4-sql/migrate/helpers/migrateValues.js
@@ -1,4 +1,17 @@
 const { cloneDeepWith, camelCase, isObject, isString } = require("lodash");
+const fs = require("fs");
+
+let apiEntityName = null;
+const apiEntityNameFilePath = "config/apiEntityName.json";
+
+function getApiEntityName() {
+  if (fs.existsSync(apiEntityNameFilePath)) {
+    apiEntityName = JSON.parse(fs.readFileSync(apiEntityNameFilePath).toString());
+    console.log(`api: `, apiEntityName);
+  } else {
+    apiEntityName = [];
+  }
+}
 
 function migrateUids(uid) {
   if (!uid) {
@@ -12,6 +25,19 @@ function migrateUids(uid) {
     "plugin::users-permissions"
   );
   result = result.replace("plugins::", "plugin::");
+
+  let matchIfApi = result.match(/(?<=api::)(\w+)\.+(\w+)$/);
+  if (matchIfApi) {
+    matchIfApi.shift();
+    if (matchIfApi[0] !== matchIfApi[1]) return result;
+    if (apiEntityName === null) getApiEntityName();
+
+    const matchedItem = apiEntityName.filter((item) => item.hasOwnProperty(matchIfApi[0]));
+
+    if (matchedItem.length)
+      return result = `api::${matchedItem[0][matchIfApi[0]]}.${matchedItem[0][matchIfApi[0]]}`;
+
+  }
   return result;
 }
 
